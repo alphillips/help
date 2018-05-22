f2539f79ce87a53f01789f5afac67dd708e9e2dc
[zapjelly] <[pak.cindy@gmail.com]>
2018-05-23 08:46:42 +1000

    update

--- demo/src/index.js
+++ demo/src/index.js
@@ -23,22 +23,26 @@ class Demo extends Component {
     const text = 'Select Automatic if you like your certificate(s) to be issued after submission or Hold if you would like to preview your certificate before it is issued. Certificate Preview can be performed after submission on the REX details screen.';
     return (
       <div>
-      <h1>Help</h1>
+        <h1>Help</h1>
 
-      <h2>Basic Help</h2>
+        <h2>Basic Help</h2>
+          <p>The following options impact when your certificate(s) are generated.
+          <Help
+            text={text} />
+          </p>
+
+        <h2>Custom help label</h2>
         <p>The following options impact when your certificate(s) are generated.
-        <Help
-          text={text} />
+          <Help
+            text={text}
+            label="Tell me more..."
+            labelClass="small-des"
+            bodyClass="body-des" />
         </p>
-
-      <h2>Custom help label</h2>
-      <p>The following options impact when your certificate(s) are generated.
-        <Help
-          text={text}
-          label="Tell me more..."
-          labelClass="small-des"
-          bodyClass="body-des" />
-      </p>
+        <div className="btn-group">
+          <button className="uikit-btn uikit-btn--tertiary" onClick={this.handleBack}>Back</button>
+          <button className="uikit-btn main-btn" onClick={this.handleNext}>Next</button>
+        </div>
       </div>
     )
   }
